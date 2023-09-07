4174b6f5bab06146061560299b76aa439696c13a
Florin.Teo <Florin.Teo@live.com>
2023-09-07 08:35:45 -0700

    Playing with UTs

---
+++ ClosestNPoints/ClosestNPointsTests.java
@@ -0,0 +1,12 @@
+package ClosestNPoints;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ClosestNPointsTests {
+    @Test
+    public void test() {
+        assertTrue(true);        
+    }
+}
