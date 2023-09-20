2f8dec3233b5424f1982dd0485f4e7d7de3ca58e
Florin.Teo <Florin.Teo@live.com>
2023-09-20 15:31:51 -0700

    Generate tests

--- Personal/TestsPrep/Program.java
+++ Personal/TestsPrep/Program.java
@@ -1,22 +1,21 @@
 package TestsPrep;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
 
-    private static final String _root = "O:/Florin.Teo@Inproted/TEALS_LWHS/LWHS CS.23-24 - Documents/General/AP CS-A/ap/unit1";
+    private static final String _root = "D:/ODrive/TEALS_LWHS/LWHS CS.23-24 - Documents/General/AP CS-A/ap/unit1";
 
     private static Path _rootP = Paths.get(_root);
-    private static Path _templateP = Paths.get(_rootP.toAbsolutePath() + "/.template");
-    private static Path _headerP = Paths.get(_templateP.toAbsolutePath() + "/.header.html");
-    private static Path _questionP = Paths.get(_templateP.toAbsolutePath() + "/.question.html");
-
+    private static Path _templateP = Paths.get(_rootP.toAbsolutePath().toString(), ".template");
 
     private static ArrayList<String> getQList() throws IOException {
         ArrayList<String> qList = new ArrayList<String>();
@@ -38,24 +37,64 @@ public class Program {
         return sList;
     }
 
-    private static void genTest(String name, ArrayList<String> qList) throws IOException {
-        Path testP = Paths.get(_rootP.toAbsolutePath() + "/" + name);
+    private static void genTestMeta(String testName) throws IOException {
+        List<String> qList = shuffle(getQList());
+        Path testP = Paths.get(_root , testName);
         Files.createDirectories(testP);
-        genTestMeta(testP, qList);
-    }
-
-    private static void genTestMeta(Path testP, ArrayList<String> qList) throws IOException {
-        Path testMetaP = Paths.get(testP.toAbsolutePath() + "/.meta");
+        Path testMetaP = Paths.get(_root, testName, ".meta");
         BufferedWriter bw = Files.newBufferedWriter(testMetaP);
+        String[] aList = {"a", "b", "c", "d", "e"};
         for(String q : qList) {
-            bw.write(q + "\n");
+            bw.write(q+" ");
+            ArrayList<String> sAList = shuffle(new ArrayList<String>(Arrays.asList(aList)));
+            for(String a: sAList) {
+                bw.write(a);
+            }
+            bw.write("\n");
         }
         bw.close();
     }
 
+    private static void genTestIndex(String testName) throws IOException {
+        List<String> metaList = loadMeta(testName);
+        String tHeader = loadTemplate(".header.html");
+        String tQuestion = loadTemplate(".question.html");
+
+        Path testIndexP = Paths.get(_root, testName, "index.html");
+        BufferedWriter bw = Files.newBufferedWriter(testIndexP);
+        bw.write(tHeader);
+        int nQ = 1;
+        for (String metaLine : metaList) {
+            String[] metaParts = metaLine.split(" ");
+            String hQuestion = tQuestion.replaceFirst("#N#", "" + nQ++);
+            hQuestion = hQuestion.replaceAll("Q#", metaParts[0]);
+            for(char c : metaParts[1].toCharArray()) {
+                hQuestion = hQuestion.replaceFirst(metaParts[0]+"#", metaParts[0] + c);
+            }
+            bw.write(hQuestion);
+        }
+        bw.close();
+    }
+
+    private static List<String> loadMeta(String testName) throws IOException {
+        Path testMetaP = Paths.get(_root, testName, ".meta");
+        return Files.readAllLines(testMetaP);
+    }
+
+    private static String loadTemplate(String templateFile) throws IOException {
+        Path fileP = Paths.get(_templateP.toAbsolutePath().toString(), templateFile);
+        List<String> lines = Files.readAllLines(fileP);
+        return String.join("\n", lines);
+    }
+
     public static void main(String[] args) throws IOException {
-        ArrayList<String> qList = getQList();
-        ArrayList<String> sqList = shuffle(qList);
-        genTest("v1", sqList);
+        genTestMeta("v1");
+        genTestIndex("v1");
+        genTestMeta("v2");
+        genTestIndex("v2");
+        genTestMeta("v3");
+        genTestIndex("v3");
+        genTestMeta("v4");
+        genTestIndex("v4");
     }
 }
