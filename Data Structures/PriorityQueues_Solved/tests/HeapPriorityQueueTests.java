657abc80bb2acb3e824f9672341f1f3627a26b49
Florin.Teo <Florin.Teo@live.com>
2024-04-16 22:53:39 -0700

    Add tests and point PQ

--- Data Structures/PriorityQueues_Solved/tests/HeapPriorityQueueTests.java
+++ Data Structures/PriorityQueues_Solved/tests/HeapPriorityQueueTests.java
@@ -7,10 +7,11 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import PriorityQueues_Solved.main.HeapPriorityQueue;
+import PriorityQueues_Solved.main.Point;
 
 public class HeapPriorityQueueTests {
     @Test
-    public void HeapPQ_tests() {
+    public void HeapPQ_IntegerTests() {
         HeapPriorityQueue<Integer> intPQ = new HeapPriorityQueue<Integer>(Integer.class);
         intPQ.add(15);
         assertEquals(15, (int)intPQ.peek());
@@ -24,5 +25,20 @@ public class HeapPriorityQueueTests {
         assertEquals(20, (int)intPQ.remove());
         assertNull(intPQ.remove());
         assertTrue(intPQ.isEmpty());
+    }
+
+    @Test
+    public void HeapPQ_PointTests() {
+        HeapPriorityQueue<Point> pointPQ = new HeapPriorityQueue<>(Point.class);
+        pointPQ.add(new Point(1, 1));
+        pointPQ.add(new Point(2, 2));
+        pointPQ.add(new Point(3, 3));
+        assertEquals("(1, 1)", pointPQ.peek().toString());
+        pointPQ.add(new Point(1, 0));
+        assertEquals("(1, 0)", pointPQ.peek().toString());
+        assertEquals("(1, 0)", pointPQ.remove().toString());
+        assertEquals("(1, 1)", pointPQ.remove().toString());
+        assertEquals("(2, 2)", pointPQ.remove().toString());
+        assertEquals("(3, 3)", pointPQ.peek().toString());
     }
 }
