96ac5d9d24f9088da5299d88c1387b64edc02674
Florin.Teo <Florin.Teo@live.com>
2024-04-20 12:19:47 -0700

    Polishing

--- Data Structures/AStar/tests/GraphTests.java
+++ Data Structures/AStar/tests/GraphTests.java
@@ -2,10 +2,7 @@ package AStar.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import java.io.FileNotFoundException;
-
 import org.junit.Test;
 
 import AStar.main.Graph;
