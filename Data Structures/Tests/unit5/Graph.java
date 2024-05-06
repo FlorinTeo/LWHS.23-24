b9ee8994bc4470275bde83661ac0bddbcbdc42d9
Florin.Teo <Florin.Teo@live.com>
2024-05-05 22:58:57 -0700

    Add U5 Test code

---
+++ Data Structures/Tests/unit5/Graph.java
@@ -0,0 +1,205 @@
+package Tests.unit5;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Class definition for a generic (Directed) Graph.
+ * The Graph contains a collection of Nodes, each Node contains
+ * a collection of references (edges) to their neighboring Nodes.
+ * @param <T> - reference type of Nodes contained in the Graph.
+ * The type T is expected to implement the Comparable interface, 
+ * such that Nodes can be compared to each other.<br>
+ * E.g.:<pre>Graph&lt;String&gt; g = new Graph&ltString&gt();</pre>
+ * @see Node
+ */
+public class Graph<T extends Comparable<T>> {
+
+    /**
+     * Private Map keying each Node in the Graph by the hashCode of its data
+     * E.g: Given <pre>Node<String> n = new Node<String>("abc");</pre> added to the graph,
+     * the _nodes map contains a Map.Entry with
+     * <pre>key="abc".hashCode()<br>value=n<pre>
+     * @see java.lang.Object#hashCode()
+     */
+    private Map<Integer, Node<T>> _nodes;
+    
+    /**
+     * Constructs a new Graph as an empty container fit for Nodes of the type T.
+     * @see Graph
+     * @see Node
+     */
+    public Graph() {
+        _nodes = new TreeMap<Integer, Node<T>>();
+    }
+    
+    /**
+     * Gets the size of this Graph. The size of the Graph is equal to the number
+     * of Nodes it contains.
+     * @return number of Nodes in this Graph.
+     */
+    public int size() {
+        return _nodes.size();
+    }
+    
+    /**
+     * Checks if the state of all the Nodes in the Graph matches a given value.
+     * @param state - the value to check against all Nodes in the Graph.
+     * @return true if all the Nodes in the Graph have a state matching the
+     * given value, false otherwise.
+     * @see Node#getState()
+     */
+    public boolean checkState(int state) {
+        for (Node<?> n : _nodes.values()) {
+            if (state != n.getState()) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Adds a new Node to the Graph containing the <i>data</i>. The method 
+     * throws if the Graph already contains a Node with data having the same
+     * hashCode().
+     * @param data - the data reference (of type T) contained in the new Node.
+     * @throws RuntimeException if the Graph already contains a Node for the
+     * given data.
+     * @see java.lang.Object#hashCode()
+     */
+    public void addNode(T data) {
+        int nodeHash = data.hashCode();
+        if (_nodes.containsKey(nodeHash)) {
+            throw new RuntimeException("Ambiguous graph!");
+        }
+        
+        _nodes.put(nodeHash, new Node<T>(data));
+    }
+    
+    /**
+     * Adds a new directed Edge to the Graph, linking the Nodes containing
+     * <i>from</i> and <i>to</i> data. It is expected the two Nodes exist
+     * otherwise the method throws an exception.
+     * @param from - Node where the Edge is starting.
+     * @param to - Node where the Edge is ending.
+     * @throws RuntimeException if either of the two Nodes are not present in the Graph.
+     * @see Node
+     * @see Graph#removeEdge(Comparable, Comparable)
+     */
+    public void addEdge(T from, T to) {
+        Node<T> fromNode = _nodes.get(from.hashCode());
+        Node<T> toNode = _nodes.get(to.hashCode());
+        if (fromNode == null || toNode == null) {
+            throw new RuntimeException("Node(s) not in the graph!");
+        }
+        
+        fromNode.addEdge(toNode);
+    }
+    
+    /**
+     * Removes an existent directed Edge from the Graph, if one exists. 
+     * The Edge to be removed is linking the nodes containing the <i>from</i> 
+     * and <i>to</i> data references. The two Nodes must exist, otherwise the 
+     * method throws an exception.
+     * @param from - Node at the starting point of the Edge.
+     * @param to - Node at the ending point of the Edge.
+     * @throws RuntimeException if either of the two Nodes are not present in the Graph.
+     * @see Node
+     * @see Graph#addEdge(Comparable, Comparable)
+     */
+    public void removeEdge(T from, T to) {
+        Node<T> fromNode = _nodes.get(from.hashCode());
+        Node<T> toNode = _nodes.get(to.hashCode());
+        if (fromNode == null || toNode == null) {
+            throw new RuntimeException("Node(s) not in the graph!");
+        }
+        
+        fromNode.removeEdge(toNode);
+    }
+    
+    /**
+     * Removes a Node from the Graph if one exists, along with all
+     * its outgoing (egress) and incoming (ingress) edges. If there
+     * is no Node hosting the <i>data</i> reference the method does
+     * nothing.
+     * @param data - Node to be removed from the Graph.
+     */
+    public void removeNode(T data) {
+        Node<T> node = _nodes.get(data.hashCode());
+        for(Node<T> n : _nodes.values()) {
+            n.removeEdge(node);
+        }
+        _nodes.remove(data.hashCode());
+    }
+    
+    /**
+     * Gives a multi-line String representation of this Graph. Each line in the returned
+     * string represent a Node in the graph, followed by its outgoing (egress) Edges.
+     * E.g: If the graph contains 3 nodes, A, B an C, where A and B point to each other and
+     * both of them point to C, the value returned by toString() will be as follows:
+     * <pre>
+     * A > B C
+     * B > A C
+     * C > 
+     * </pre>
+     * <u>Note:</u> Each line is a space-separated sequence of token. A Node with no
+     * outgoing (egress) edges, like C in the example above still has a line where 
+     * the ' > ' token is surrounded by the space characters.
+     * @return multi-line String reflecting the content and structure of this Graph.
+     */
+    @Override
+    public String toString() {
+        String output = "";
+        boolean first = true;
+        for(Node<?> n : _nodes.values()) {
+            if (!first) {
+                output += "\n";
+            }
+            output += n.toString();
+            first = false;
+        }
+        
+        return output;
+    }
+    
+    public void reset() {
+        reset(0);
+    }
+    
+    public void reset(int value) {
+        for(Node<T> node : _nodes.values()) {
+            node.reset(value);
+        }
+    }
+    
+    public int countUndirected() {
+        int count = 0;
+        for(Node<T> n : _nodes.values()) {
+            count += n.countUndirected();
+        }
+        
+        return count/2;   
+    }
+    
+    public int countHops(T from, T to) {
+        Node<T> fromN = _nodes.get(from.hashCode());
+        Node<T> toN = _nodes.get(to.hashCode());
+        if (fromN == null || toN == null) {
+            return -1;
+        } else {
+            reset(0);
+            return fromN.countHops(toN);
+        }
+    }
+
+    public boolean hasPath(T from, T to) {
+        Node<T> fromN = _nodes.get(from.hashCode());
+        Node<T> toN = _nodes.get(to.hashCode());
+        if (fromN == null || toN == null) {
+            throw new RuntimeException();
+        }
+        reset(0);
+        return fromN.hasPath(toN);
+    }
+}
---
+++ Data Structures/Tests/unit5/TestsCore.java
@@ -0,0 +1,115 @@
+package Tests.unit5;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class TestsCore {
+    
+    private Scanner getScanner(String graphFile) throws FileNotFoundException {
+        URL url = this.getClass().getResource(graphFile);
+        File file = new File(url.getFile());
+        String filePath = file.getAbsolutePath();
+        try {
+            filePath = java.net.URLDecoder.decode(filePath, StandardCharsets.UTF_8.name());
+        } catch (Exception e) {
+        }
+        System.out.println(file.getAbsolutePath());
+        return new Scanner(new File(filePath));
+    }
+    
+    public <T extends Comparable<T>> T parseT(String s, Class<T> realType) {
+        s = s.trim();
+        if (realType == Integer.class) {
+            return realType.cast(Integer.parseInt(s));
+        } else if (realType == String.class) {
+            return realType.cast(s);
+        } else if (realType == Double.class) {
+            return realType.cast(Double.parseDouble(s));
+        } else if (realType == Character.class) {
+            if (s.length() != 1) {
+                throw new RuntimeException("Invalid format in graph parsing!");
+            }
+            return realType.cast(s.charAt(0));
+        } else {
+            throw new RuntimeException("Unsupported type in graph parsing!");
+        }
+    }
+
+    public <T extends Comparable<T>> Graph<T> readGraph(String graphFile, Class<T> realType) throws FileNotFoundException {
+        Scanner input = getScanner(graphFile);
+        Map<String, List<String>> linksMap = new HashMap<String, List<String>>();
+        Map<String, T> nodesMap = new HashMap<String, T>();
+        while(input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] tokens = line.split(">");
+            if (tokens.length < 1) {
+                input.close();
+                throw new RuntimeException("Syntax error in parsing graph!");
+            }
+            String[] data = tokens[0].trim().split("\\s+:\\s+");
+            String[] links = tokens.length > 1
+                ? tokens[1].trim().split("\\s+")
+                : new String[0];
+            T n = parseT(tokens[0], realType);
+            linksMap.put(data[0], Arrays.asList(links));
+            nodesMap.put(data[0], n);
+        }
+        input.close();
+        
+        Graph<T> graph = new Graph<T>();
+        for(T n : nodesMap.values()) {
+            graph.addNode(n);
+        }
+        
+        for(Map.Entry<String, T> kvp : nodesMap.entrySet()) {
+            T fromNode = kvp.getValue();
+            for(String v : linksMap.get(kvp.getKey())) {
+                T toNode= nodesMap.get(v);
+                if (toNode != null) {
+                    graph.addEdge(fromNode, toNode);
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    public Graph<String> readGraph(String graphFile) throws FileNotFoundException {
+        return readGraph(graphFile, String.class);
+    }
+    
+    public void assertSameGraph(String graphFile, Graph<?> g) throws FileNotFoundException {
+        Scanner parser = getScanner(graphFile);
+        Set<String> expected = new TreeSet<String>();
+        Set<String> actual = new TreeSet<String>();
+        while(parser.hasNextLine()) {
+            String line = parser.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            expected.add(line);
+        }
+        for(String line : g.toString().split("\n")) {
+            actual.add(line);
+        }
+        parser.close();
+        assertEquals(expected.size(), actual.size());
+        Iterator<String> iExpected = expected.iterator();
+        Iterator<String> iActual = actual.iterator();
+        while(iExpected.hasNext()) {
+            assertSameNode(iExpected.next(), iActual.next());
+        }
+    }
+
+    public void assertSameNode(String expectedNode, String actualNode) {
+        Set<String> expectedTokens = new TreeSet<String>(Arrays.asList(expectedNode.split("\\s+")));
+        Set<String> actualTokens = new TreeSet<String>(Arrays.asList(actualNode.split("\\s+")));
+        assertTrue(expectedTokens.equals(actualTokens));
+    }
+}
