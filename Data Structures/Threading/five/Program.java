baae1101290a73e4d6a00880e5c1685adb237535
Florin.Teo <Florin.Teo@live.com>
2024-06-09 12:01:56 -0700

    Add five

---
+++ Data Structures/Threading/five/Program.java
@@ -0,0 +1,16 @@
+package Threading.five;
+
+public class Program {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello MyFirstThreads!");
+        SharedInt sharedInt = new SharedInt();
+        Thread t1 = new Thread(new IncRunnable(sharedInt));
+        Thread t2 = new Thread(new IncRunnable(sharedInt));
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(sharedInt.getNum());
+        System.out.println("Goodbye!");
+    }
+}
---
+++ Data Structures/Threading/five/SharedInt.java
@@ -0,0 +1,13 @@
+package Threading.five;
+
+public class SharedInt {
+    private int _number = 0;
+
+    public int getNum() {
+        return _number;
+    }
+
+    public void setNum(int number) {
+        _number = number;
+    }
+}
